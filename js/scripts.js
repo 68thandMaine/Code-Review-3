2be3f34c3021deed87d040adf06fdbff73861636
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-10-26 08:48:55 -0700

    initial commit with some function shells set up

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
-$(document).ready(funtion() {
+function replace0(number) {
+  //This function will take a number
+  //convert it to a string
+  var toString = number.split('');
+  // determine if the number contains a zero
+console.log(toString);
+
+//and return the word beep
+};
 
 
 
+$(document).ready(function(){
 
-
-
-)};
+$("form#form1").submit(function(event) {
+var number = parseInt($("#userInput").val());
+alert (number)
+});
+});
