0e6d3f256d55d855d1a666999edecf381e85342b
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-10-26 15:33:38 -0700

    debug beep and boop functions

--- js/scripts.js
+++ js/scripts.js
@@ -4,54 +4,66 @@ function count(num) {
   for (var i = 0; i < num; i++) {
     result += i;
   }
+  console.log(result)
   return result.split('')
 }
 
 //This function will take a parameter and evaluate if the number it is given as an arguement contains 0. If it does then it will change the number to Beep. The function accomplishes this by using a method called .includes to evaluate whether or not the parameter contains the character 0. If there is a 0 in the string, then the empty variable result will be passed the value "beep". IF there is a 1 in the string then, using the same logic, the function will return the work Beep.
-function beepBoop(num) {
-  var result = " ";
+function beep(num) {
+  var holder = num
+  if (num.includes('1')) {
+    return "Beep";
+  } else {
+    return holder
+  };
+  };
+
+
+function boop(num) {
+  var holder = num;
   if (num.includes('0')) {
-    result = "Beep"
-  } else if (num.includes('1')) {
-    result = "Boop"
+    return "Boop";
+  } else {
+    return holder
   }
+  console.log(result)
   return result
-}
+  };
+
+
 
 //This function will take a number and determine if it is divisible by 3. This is accomplished by checking to see if the parameter has a remainder of 0 when it is divided by 3. If true the result displays the text. If false the number is returned. DOES NOT WORK FOR MULTIPLE ENTRIES
 function divBy3(num) {
   var result = [];
   if (num % 3 === 0) {
-    result.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    return "I'm sorry, Dave. I'm afraid I can't do that.";
   } else {
     result.push(num);
   }
   return result
 }
-
-
 //This function will take the user input and translate it into the appropriate message depending on the range of numbers.
 
 //Not returning  information correctly if number is larger than 10
 function machine(num) {
   var result = [];
   var arr = count(num)
-  for (var i = 0; i < arr.length; i++) {
-    if (i === 0) {
-      result.push(" beep")
-    } else if (i === 1) {
-      result.push(" boop")
-    }
-    else if (i % 3 === 0) {
-      result.push(" I'm sorry, Dave. I'm afraid I can't do that")
-    } else {
-      result.push(" " +i )
-    }
+  for (var i = 0; i <= arr.length; i++) {
 
+    if (i % 3 === 0) {
+      result.push(" I'm sorry, Dave. I'm afraid I can't do that")
+    } else if (i.includes('1')){
+      var beeps = beep(num)
+      result.push(beeps);
+    } else if (i.includes('0')){
+      result.push(" " + boop(num));
+    };
+    console.log(arr)
+    console.log(result)
+    return result
   };
-  console.log(result)
-return result
-}
+
+};
 
 
 
