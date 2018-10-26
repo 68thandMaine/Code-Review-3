0fd22c8495cd526ea2610ccedd2e2bdc7051cc0d
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-10-26 11:13:26 -0700

    add function Beep that will return beep if a number contains 0

--- README.md
+++ README.md
@@ -10,6 +10,12 @@ This program will ask the user to input a number. The number is run through seve
 
 Specs:
 
-Behavior: User inputs a number and a range of numbers is created up to the inputted number.
+Behavior 1:
+The user inputs a number and a range of numbers is created up to the inputted number.
 Input: 6
 Expected output: 0,1,2,3,4,5,6
+
+Behavior 2:
+A number that contains 0 should be outputted as the string "Beep"
+Input: 101
+Expected output: Beep
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// This function will create a range of numbers by taking a number as a parameter (num) and running it through a for loop. By starting the index at the value 1, the for loop places a range of numbers (listed as one string eg 1234 not 1, 2, 3, 4) by adding the value of i to the result array as the loop moves from 1 to num.
+// This function will create a range of numbers by taking a number as a parameter (num) and running it through a for loop. By starting the index at 1 instead of 0, the for loop places a range of numbers (listed as one string eg 1234 not 1, 2, 3, 4) by adding the value of i to the result array as the loop moves from 1 to num. If i = 0 then the list would be 01234.
 function replace0(num) {
   var result = [];
   for (var i = 1; i < num; i++) {
@@ -7,11 +7,20 @@ function replace0(num) {
   return result + num;
 }
 
+//This function will take a parameter and evaluate if the number it is given as an arguement contains 0. If it does then it will change the number to Beep. The function accomplishes this by using a method called .includes to evaluate whether or not the parameter contains the character 0. If there is a 0 in the string, then the empty variable result will be passed the value "beep"
+function beep(num) {
+  var result = " ";
+    if (num.includes('0')) {
+      result = "Beep"
+    }
+    return result
+}
+
+
 $(document).ready(function() {
   $("#form1").submit(function(event) {
     event.preventDefault();
     var number = $("#userInput").val();
-    $('#results').text(replace0(number));
-    alert(number)
+    $('#results').text(beep(number));
   });
 });
