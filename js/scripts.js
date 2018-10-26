6b053a6ddd682e7655261d146385f3431b2bdd08
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-10-26 10:24:08 -0700

    add a range counter in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,17 @@
-function replace0(number) {
-  //This function will take a number
-  //convert it to a string
-  var toString = number.split('');
-  // determine if the number contains a zero
-console.log(toString);
+// This function will create a range of numbers by taking a number as a parameter (num) and running it through a for loop. By starting the index at the value 1, the for loop places a range of numbers (listed as one string eg 1234 not 1, 2, 3, 4) by adding the value of i to the result array as the loop moves from 1 to num.
+function replace0(num) {
+  var result = [];
+  for (var i = 1; i < num; i++) {
+    result += i;
+  }
+  return result + num;
+}
 
-//and return the word beep
-};
-
-
-
-$(document).ready(function(){
-
-$("form#form1").submit(function(event) {
-var number = parseInt($("#userInput").val());
-alert (number)
-});
+$(document).ready(function() {
+  $("#form1").submit(function(event) {
+    event.preventDefault();
+    var number = $("#userInput").val();
+    $('#results').text(replace0(number));
+    alert(number)
+  });
 });
